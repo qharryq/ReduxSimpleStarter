902d9db0fe60637f2fd16212322b9297075ac9c4
harryq <harryq@openet.com>
2017-07-20 16:48:17 +0100

    lesson 12

--- src/index.js
+++ src/index.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+//arrow syntax is cleaner and makes this keyword have a different value compared to normal function syntax
+const App = () => {
+  return <div>Hi!</div>;
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
-
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+//putting App inside tag creates an instance of the class
+ReactDOM.render(<App />, document.querySelector('.container'));
