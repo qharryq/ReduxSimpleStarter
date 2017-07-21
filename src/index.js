98eef48ed7c5d9943ce65a2946259d053f8a354d
harryq <harryq@openet.com>
2017-07-21 16:30:52 +0100

    lesson 14

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyBdHuat6xcid48svtGgfUFaWzccRB9IoBc';
+
 //arrow syntax is cleaner and makes this keyword have a different value compared to normal function syntax
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+      <SearchBar />
+    </div>
+  );
 }
 
 //putting App inside tag creates an instance of the class
