6f8525c7902b2b9680885c468d9c3e5e8edb2bde
harryq <harryq@openet.com>
2017-08-14 13:55:04 +0100

    small progress

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,7 +1,11 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const SearchBar = () => {
-    return <input />;
+//use class component instead of functional componenet when you need more functionality, i.e. component needs to be more aware
+//class componenets need render method, functional do not
+class SearchBar extends Component {
+    render() {
+        return <input />;
+    }
 }
 
 export default SearchBar;
